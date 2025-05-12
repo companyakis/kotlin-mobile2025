05bb9469e75f34121d63b7ecf1298874d672c803
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-05-12 10:43:24 +0300

    Create part-z-0001-basic-types2.kt

---
+++ part-z-0001-basic-types2.kt
@@ -0,0 +1,14 @@
+fun main() {
+    
+    var pi1: Double = 22.0 / 7.0
+    
+    var pi2: Float = 22.0F / 7.0F
+    
+    println("Double Pi: $pi1")
+    
+	  println("Float Pi: $pi2")
+    
+}
+
+// Double Pi: 3.142857142857143
+// Float Pi: 3.142857

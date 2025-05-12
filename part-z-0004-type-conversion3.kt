91412736b312e4da87199909243a5a93441c3ded
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-05-12 12:48:37 +0300

    Create part-z-0004-type-conversion3.kt

---
+++ part-z-0004-type-conversion3.kt
@@ -0,0 +1,28 @@
+fun main() {
+    
+    var x: String = "100"
+    
+    var y: String = "120"
+    
+    var z: String = "120,12"
+    
+    var x1 = x.toIntOrNull()
+    
+	  var y1 = y.toIntOrNull()
+    
+    var z1 = z.toIntOrNull()
+    
+    if(x1 != null && y1 != null && z1 != null) {
+        
+        val result1 = x1 + y1 + z1
+        
+        println(result1)
+    }
+    
+    else {
+   
+        
+        println("Type conversion error!..") // Type conversion error!..
+    }
+    
+}

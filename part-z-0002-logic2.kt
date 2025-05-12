aa8510e37aa61b6a8f90cfe3a00da33e45c4792b
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-05-12 10:55:45 +0300

    Create part-z-0002-logic2.kt

---
+++ part-z-0002-logic2.kt
@@ -0,0 +1,21 @@
+fun main() {
+    
+    var cond1: Boolean = true
+    
+    var cond2: Boolean = false
+    
+    var result1: Boolean = cond1 && cond2
+    
+	var result2: Boolean = cond1 || cond2
+    
+    var result3: Boolean = (cond1 && cond2) || (cond1 || cond2)
+    
+	var result4: Boolean = (cond1 || cond2) && (cond1 && cond2)
+    
+    println(result1)
+    println(result2)
+    println(result3)
+    println(result4)
+    
+           
+}

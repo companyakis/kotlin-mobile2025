97a336011209027fd8061b708e0d6bb92acb5054
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-05-12 10:39:38 +0300

    Create part-z-0001-basic-types1.kt

---
+++ part-z-0001-basic-types1.kt
@@ -0,0 +1,21 @@
+fun main() {
+    
+    var bigNum: Long = 300_256_687_654
+    
+    var yearlySalesUSD: Int = 654_871_900 
+    
+    var startupYear: Short = 2016
+    
+    var age: Byte = 17 
+    
+    println(bigNum)
+    println(yearlySalesUSD)
+    println(startupYear)
+    println(age)
+    
+}
+
+// 300256687654
+// 654871900
+// 2016
+// 17

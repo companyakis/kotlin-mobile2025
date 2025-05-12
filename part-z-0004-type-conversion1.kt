100f1a775bc0c4b2876412113aa6fd93306ca7af
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-05-12 12:24:36 +0300

    Create part-z-0004-type-conversion1.kt

---
+++ part-z-0004-type-conversion1.kt
@@ -0,0 +1,20 @@
+fun main() {
+    
+    var x: Int = 100
+    
+    var y: String = "200"
+    
+    var y1 = x.toString()
+    
+  	var x1 = x.toDouble()
+    
+    var x2 = x.toFloat()
+    
+    var x3 = x.toLong()
+    
+    var x4 = x.toShort()
+    
+    var x5 = x.toByte()
+     
+
+}

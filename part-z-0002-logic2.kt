ad2355edadb46eeceae418845db741f8adbcbadd
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-05-12 10:56:04 +0300

    Update part-z-0002-logic2.kt

--- part-z-0002-logic2.kt
+++ part-z-0002-logic2.kt
@@ -6,11 +6,11 @@ fun main() {
     
     var result1: Boolean = cond1 && cond2
     
-	var result2: Boolean = cond1 || cond2
+    var result2: Boolean = cond1 || cond2
     
     var result3: Boolean = (cond1 && cond2) || (cond1 || cond2)
     
-	var result4: Boolean = (cond1 || cond2) && (cond1 && cond2)
+    var result4: Boolean = (cond1 || cond2) && (cond1 && cond2)
     
     println(result1)
     println(result2)

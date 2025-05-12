974985adaae4f43b4f83a1528e90f3a1696582fb
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-05-12 10:51:58 +0300

    Create part-z-0002-logic1.kt

---
+++ part-z-0002-logic1.kt
@@ -0,0 +1,22 @@
+fun main() {
+    
+    var cond1: Boolean = 5 > 3
+    
+    var cond2: Boolean = 5 >= 3
+    
+    var cond3: Boolean = 5 < 3
+    
+    var cond4: Boolean = 5 <= 3
+    
+    var cond5: Boolean = 5 == 3
+    
+    var cond6: Boolean = 5 != 3
+    
+    println(cond1)
+    println(cond2)
+    println(cond3)
+    println(cond4)
+    println(cond5)
+    println(cond6)
+  
+}

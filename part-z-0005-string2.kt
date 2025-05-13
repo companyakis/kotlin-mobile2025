5f832f6139d56ce29f64dc1c53bb08ebb3ec9232
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-05-13 10:54:04 +0300

    Create part-z-0005-string2.kt

---
+++ part-z-0005-string2.kt
@@ -0,0 +1,17 @@
+fun main() {
+    
+    var userInput = """
+    
+    		Hi. Under the assumption of pure competition...
+        	
+    Lorem ipsum...
+    
+    """
+    
+	println(userInput)
+    
+    println("----------------------------")
+    
+    println(userInput.trimIndent())
+	 
+}

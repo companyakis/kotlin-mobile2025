b6d93474b6e21f17f3889190b1f8987dd0021cd2
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-05-13 10:49:38 +0300

    Create part-z-0005-string1.kt

---
+++ part-z-0005-string1.kt
@@ -0,0 +1,19 @@
+fun main() {
+    
+    var proverb = String()
+    
+    proverb = "Out of sight, out of mind!"
+    
+	  var txt = ""
+    
+    if (txt.isEmpty()) {
+        
+        println("Empty text!")
+    }
+    
+    else {
+        
+        println(txt)
+    }
+	 
+}

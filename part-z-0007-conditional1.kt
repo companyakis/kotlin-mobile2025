86d7424b8c5a5379b5028a58c30f3d33d4fb7408
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-05-15 19:51:27 +0300

    Create part-z-0007-conditional1.kt

---
+++ part-z-0007-conditional1.kt
@@ -0,0 +1,20 @@
+fun main() {
+    
+    val a: Byte = 12
+    
+    val b: Byte = 12
+    
+    if (a > b) {
+        
+        println("$a > $b")
+        
+    } else if (b > a) {
+        
+        println("$b > $a")
+        
+    } else {
+        
+        println("$a = $b")
+    }
+     
+}
